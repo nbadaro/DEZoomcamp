3f4599271cfb9caff19e5b61164bd09d75c5f533
techsieve <tech@trysieve.com>
2024-01-23 10:48:20 +0000

    persist homework queries and answers

--- week_1_docker_terraform/2_sql_docker/homework.sql
+++ week_1_docker_terraform/2_sql_docker/homework.sql
@@ -12,6 +12,7 @@ SELECT
 	COUNT(DISTINCT index::varchar) as unique_ids
 FROM filtered_rides;
 
+
 -- Q: Which was the pick up day with the largest trip distance. Use the pick up time for your calculations.
 -- A: 2019-09-26
 SELECT
@@ -47,6 +48,27 @@ SELECT
 	*
 FROM rides_sum
 WHERE sum_total_amount >= 50000;
+
+
+-- Q: For the passengers picked up in September 2019 in the zone name Astoria,
+-- which was the drop off zone that had the largest tip? We want the name of the zone, not the id.
+-- A: JFK Airport
+
+WITH max_tip_location AS (
+	SELECT
+		"DOLocationID" as dropoff_location_id
+	FROM public.green_trip_data
+	WHERE lpep_pickup_datetime::date >= '2019-09-01'
+	AND lpep_pickup_datetime::date <= '2019-09-30'
+	AND "PULocationID" = 7
+	ORDER BY tip_amount DESC
+	LIMIT 1
+)
+SELECT
+	z."Zone"
+FROM public.zones AS z
+JOIN max_tip_location AS mt ON mt.dropoff_location_id = z."LocationID";
+
 
 SELECT * FROM public.zones z LIMIT 10;
 SELECT * FROM public.green_trip_data LIMIT 10;
