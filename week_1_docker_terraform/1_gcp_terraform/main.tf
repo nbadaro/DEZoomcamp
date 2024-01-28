79ad5986fec26aa55c7bd19e654ee961417378c7
Nic <nbadaro@gmail.com>
2024-01-28 14:54:20 +0000

    add new resources for mage in GCP

--- week_1_docker_terraform/1_gcp_terraform/main.tf
+++ week_1_docker_terraform/1_gcp_terraform/main.tf
@@ -9,13 +9,30 @@ terraform {
 
 provider "google" {
   credentials = file(var.credentials)
-  project = var.project
-  region  = var.region
+  project     = var.project
+  region      = var.region
 }
 
 resource "google_storage_bucket" "taxi-data-bucket" {
   name          = var.gcs_bucket_name
   location      = var.location
+  storage_class = var.gcs_storage_class
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_storage_bucket" "mage-data-bucket" {
+  name          = var.gcs_mage_bucket_name
+  location      = var.location
+  storage_class = var.gcs_storage_class
   force_destroy = true
 
   lifecycle_rule {
@@ -34,5 +51,5 @@ resource "google_bigquery_dataset" "ny_taxi_dataset" {
   friendly_name               = "taxi"
   description                 = "NYC Taxi Trips Data"
   default_table_expiration_ms = 3600000
-  delete_contents_on_destroy = true
+  delete_contents_on_destroy  = true
 }
